5374ae60efe48557bbd2212eb156462a14190466
TD <trandoanmyan@gmail.com>
2024-02-28 10:08:57 +0700

    Add List Products for Admin

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,15 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+using X.PagedList;
 
 namespace StoreApp.Areas.Admin.Controllers;
 
 [Area("Admin")]
 [Route("admin")]
-[Route("admin/homeadmin")]
+// [Route("admin/homeadmin")]
 public class HomeAdminController : Controller
 {
+    private readonly QLBanVaLiContext _dbContext;
+
+    public HomeAdminController(QLBanVaLiContext dbContext) {
+        _dbContext = dbContext;
+    }
+
     [Route("")]
     [Route("index")]
     public IActionResult Index() {
         return View();
+    }
+
+    [Route("danhmucsanpham")]
+    public IActionResult DanhMucSanPham(int? page) {
+        int pageNumber = page == null || page < 1 ? 1 : page.Value;
+        int pageSize = 12;
+        var lstDanhMucSp = _dbContext.TDanhMucSps.AsNoTracking().OrderBy(item => item.TenSp);
+        PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstDanhMucSp, pageNumber, pageSize);
+        return View(lst);
     }
 }
