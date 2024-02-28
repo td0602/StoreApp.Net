1ee2e770ec2f396d4db0083605a7b70cd4156d41
TD <trandoanmyan@gmail.com>
2024-02-28 19:47:32 +0700

    Add Delete Product

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -35,7 +35,7 @@ public class HomeAdminController : Controller
     [Route("themsanphammoi")]
     [HttpGet]
     public IActionResult ThemSanPhamMoi() {
-        // khởi tạo mặc định cho 1 vài thuộc tính của sản phẩm
+        // khởi tạo mặc định cho 1 vài thuộc tính của sản phẩm để ta có thể select trong form
         ViewBag.MaChatLieu = new SelectList(_dbContext.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
         ViewBag.MaHangSx = new SelectList(_dbContext.THangSxes.ToList(), "MaHangSx", "HangSx");
         ViewBag.MaNuocSx = new SelectList(_dbContext.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
@@ -53,5 +53,48 @@ public class HomeAdminController : Controller
             return RedirectToAction("DanhMucSanPham");
         }
         return View(sanPham);
+    }
+
+    [Route("suasanpham")]
+    [HttpGet]
+    public IActionResult SuaSanPham(string maSanPham) {
+        // khởi tạo mặc định cho 1 vài thuộc tính của sản phẩm để ta có thể select trong form
+        ViewBag.MaChatLieu = new SelectList(_dbContext.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+        ViewBag.MaHangSx = new SelectList(_dbContext.THangSxes.ToList(), "MaHangSx", "HangSx");
+        ViewBag.MaNuocSx = new SelectList(_dbContext.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+        ViewBag.MaLoai = new SelectList(_dbContext.TLoaiSps.ToList(), "MaLoai", "Loai");
+        ViewBag.MaDt = new SelectList(_dbContext.TLoaiDts.ToList(), "MaDt", "TenLoai");
+        var sanPham = _dbContext.TDanhMucSps.Find(maSanPham);
+        return View(sanPham);
+    }
+    [Route("suasanpham")]
+    [HttpPost]
+    [ValidateAntiForgeryToken] //Validate du lieu
+    public IActionResult SuaSanPham(TDanhMucSp sanPham) {
+        if(ModelState.IsValid) {
+            // _dbContext.Update(sanPham);
+            _dbContext.Entry(sanPham).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+        }
+        return View(sanPham);
+    }
+
+    [Route("xoasanpham")]
+    [HttpGet]
+    public IActionResult XoaSanPham(string maSanPham) {
+        TempData["Message"] = "";
+        // kiểm tra xem chi tiết sp có tồn tại? k có mới xóa dc
+        var chiTietSps = _dbContext.TChiTietSanPhams.Where(item => item.MaSp == maSanPham).ToList();
+        if(chiTietSps.Count() > 0) {
+            TempData["Message"] = "Không được xóa sản phẩm này";
+            return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+        }
+        var anhSps = _dbContext.TAnhSps.Where(item => item.MaSp == maSanPham).ToList();
+        if(anhSps.Any()) _dbContext.RemoveRange(anhSps);
+        _dbContext.Remove(_dbContext.TDanhMucSps.Find(maSanPham));
+        _dbContext.SaveChanges();
+        TempData["Message"] = "Xóa sản phẩm thành công";
+        return RedirectToAction("DanhMucSanPham", "HomeAdmin");
     }
 }
