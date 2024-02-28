301ff5f0a9128f10115609e9a2043f6fba40b7b4
TD <trandoanmyan@gmail.com>
2024-02-28 12:26:19 +0700

    Add form Add Product

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StoreApp.Models;
 using X.PagedList;
@@ -29,5 +30,28 @@ public class HomeAdminController : Controller
         var lstDanhMucSp = _dbContext.TDanhMucSps.AsNoTracking().OrderBy(item => item.TenSp);
         PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstDanhMucSp, pageNumber, pageSize);
         return View(lst);
+    }
+
+    [Route("themsanphammoi")]
+    [HttpGet]
+    public IActionResult ThemSanPhamMoi() {
+        // khởi tạo mặc định cho 1 vài thuộc tính của sản phẩm
+        ViewBag.MaChatLieu = new SelectList(_dbContext.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+        ViewBag.MaHangSx = new SelectList(_dbContext.THangSxes.ToList(), "MaHangSx", "HangSx");
+        ViewBag.MaNuocSx = new SelectList(_dbContext.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+        ViewBag.MaLoai = new SelectList(_dbContext.TLoaiSps.ToList(), "MaLoai", "Loai");
+        ViewBag.MaDt = new SelectList(_dbContext.TLoaiDts.ToList(), "MaDt", "TenLoai");
+        return View();
+    }
+    [Route("themsanphammoi")]
+    [HttpPost]
+    [ValidateAntiForgeryToken] //Validate du lieu
+    public IActionResult ThemSanPhamMoi(TDanhMucSp sanPham) {
+        if(ModelState.IsValid) {
+            _dbContext.TDanhMucSps.Add(sanPham);
+            _dbContext.SaveChanges();
+            return RedirectToAction("DanhMucSanPham");
+        }
+        return View(sanPham);
     }
 }
